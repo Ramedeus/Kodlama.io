3cbe1f42e3393166821872d0f0839309bbf17e0d
URASS <URASS@URAS>
2022-09-19 21:06:38 +0300

    For Döngüsü !

---
+++ intro/src/forDongusu/Main.java
@@ -0,0 +1,15 @@
+package forDongusu;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti");
+
+	}
+
+}
