3866db2e39c083090baa3314e6dfb7b43d7bed38
URASS <URASS@URAS>
2022-09-19 20:43:24 +0300

    Değişkenler !

---
+++ intro/src/degiskenler/Main.java
@@ -0,0 +1,13 @@
+package degiskenler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrencisayisi = 12;
+		String mesaj="Öğrenci sayısı: ";
+		System.out.println(mesaj + ogrencisayisi);
+		
+		
+	}
+
+}
