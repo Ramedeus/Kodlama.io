84010810481494267385bc795f33c26ae4d136df
URASS <URASS@URAS>
2022-09-19 21:11:47 +0300

    Do-While Döngüsü !

---
+++ intro/src/doWhileDongusu/Main.java
@@ -0,0 +1,15 @@
+package doWhileDongusu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j = 100;
+		do {
+			System.out.println("Loglandı");
+			System.out.println(j);
+			j+=2;
+		} while (j<10);
+		System.out.println("Do-While Döngüsü Bitti");
+	}
+
+}
