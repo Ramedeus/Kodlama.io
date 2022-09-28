7502c15e7def8bdeb4a1de44bb9fa28c3af6bbcf
URASS <URASS@URAS>
2022-09-28 22:00:27 +0300

    Değer ve Referans Tipleri

---
+++ intro/src/classes_2/Main.java
@@ -0,0 +1,17 @@
+package classes_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager;
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager=customerManager2;
+		
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+		
+	}
+
+}
