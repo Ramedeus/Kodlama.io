26c46385a251d3271691417ae80e23a4c2d2bc98
URASS <URASS@URAS>
2022-09-28 20:52:52 +0300

    Metodlar 3

---
+++ intro/src/metodlar_3/Main.java
@@ -0,0 +1,47 @@
+package metodlar_3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "Bugün hava çok güzel";
+		String yeniMesaj = sehirVer();
+		System.out.println(yeniMesaj);
+		int sayi = topla(10, 5);
+		System.out.println(sayi);
+		System.out.println(topla2(1,2,3,4));
+	}
+
+	public static void ekle() {
+		System.out.println("eklendi");
+	}
+
+	public static void sil() {
+		System.out.println("silindi");
+	}
+
+	public static void guncelle() {
+		System.out.println("güncellendi");
+	}
+
+	public static int topla(int x, int y) {
+		return x + y;
+	}
+
+	public static int topla2(int...sayilar) {
+		int toplam=0;
+		
+		for (int sayi : sayilar) {
+			toplam+=sayi;
+		}
+		
+		return toplam;
+	}
+	
+	
+	public static String sehirVer() {
+		return "Ankara";
+	}
+	
+	
+
+}
