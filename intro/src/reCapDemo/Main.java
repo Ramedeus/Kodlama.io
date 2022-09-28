2f113af20781ade49b2bf68889de0342fae42c84
URASS <URASS@URAS>
2022-09-28 21:59:59 +0300

    ReCap Demo Classlar

---
+++ intro/src/reCapDemo/Main.java
@@ -0,0 +1,13 @@
+package reCapDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.Topla(2, 2));
+		System.out.println(dortIslem.Cikar(2, 2));
+		System.out.println(dortIslem.Carp(2, 3));
+		System.out.println(dortIslem.Bol(20, 5));
+	}
+
+}
