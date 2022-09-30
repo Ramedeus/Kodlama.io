fff3e96bfe237b250ecb1920b7d61b39ec3a032e
URASS <URASS@URAS>
2022-09-30 20:18:12 +0300

    inheritence2

--- intro/src/inherience/Main.java
+++ intro/src/inherience/Main.java
@@ -3,14 +3,14 @@ package inherience;
 public class Main {
 
 	public static void main(String[] args) {
-		Customer customer=new Customer();
-		Employee employee=new Employee();
-		
-		EmployeeManager employeeManager= new EmployeeManager();
-		CustomerManager customerManager=new CustomerManager();
-		
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+
+		EmployeeManager employeeManager = new EmployeeManager();
+		CustomerManager customerManager = new CustomerManager();
+
 		customerManager.Add();
 		employeeManager.BestEmployee();
-}
+	}
 
 }
