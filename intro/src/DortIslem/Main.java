31da86d1e7ad128505bb94e56c2fbc6a91ba46aa
URASS <URASS@URAS>
2022-09-30 19:59:38 +0300

    Method Overloading

---
+++ intro/src/DortIslem/Main.java
@@ -0,0 +1,10 @@
+package DortIslem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(1, 4,5));
+	}
+
+}
