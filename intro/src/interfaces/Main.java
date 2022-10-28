0a9775fb3447b7ef6c44b96d13b5c7fb9e6ead2a
URASS <URASS@URAS>
2022-10-28 15:36:18 +0300

    interfaces

--- intro/src/interfaces/Main.java
+++ intro/src/interfaces/Main.java
@@ -3,8 +3,8 @@ package interfaces;
 public class Main {
 
 	public static void main(String[] args) {
-		ICustomerDal customerDal = new OracleCustomerDal();
-		customerDal.Add();
+		CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
+		customerManager.add();
 	}
 
 }
