418a02dac8aceb25076f6a598fc014d8266775d6
URASS <URASS@URAS>
2022-10-28 18:44:02 +0300

    static

--- intro/src/staticDemo/Main.java
+++ intro/src/staticDemo/Main.java
@@ -1,17 +1,16 @@
 package staticDemo;
 
 public class Main {
-	
-	public static void main (String[] args) {
-		ProductManager productManager=new ProductManager();
-		Product product=new Product();
-		
-		product.name="";
-		product.price=100;
-		
+
+	public static void main(String[] args) {
+		ProductManager productManager = new ProductManager();
+		Product product = new Product();
+
+		product.name = "";
+		product.price = 100;
+
 		productManager.add(product);
-		
+
 	}
-	
-	
+
 }
